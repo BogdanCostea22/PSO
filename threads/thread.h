2d46554df57570acd79dd626b93082661999230b
BogdanCostea22 <bogdan.costea@gmail.com>
2019-11-30 21:15:04 +0200

    Change Thread structure

--- threads/thread.h
+++ threads/thread.h
@@ -89,7 +89,8 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
-
+    int basePriority; //Will store the base priority of this thread
+    struct lock waitForLock; //This thread is waiting for this lock
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
